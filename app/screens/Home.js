26a9044e69b73cec47492e143c606a9f6273303a
julielaursen <julielaursen1@gmail.com>
2020-02-10 15:38:40 -0600

    start of creating currency converter app

---
+++ app/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { Container } from '../components/Container';
+
+import { Container } from '../components/Container';
+
+export default () => (
+<Container>
+    <StatusBar translucent = {false} barStyle="light-content"/>
+    <View />
+</Container>
+
+);
